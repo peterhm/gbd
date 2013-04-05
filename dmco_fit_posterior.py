34ebfe3a5dd7bf44de26bb47f8f9728199554db6
Hannah Peterson <peterhm@uw.edu>
2013-04-05 15:53:31 -0700

    initial commit of posterior (prev dmco_run)

---
+++ dmco_fit_posterior.py
@@ -0,0 +1,41 @@
+import dismod3
+import fit_posterior 
+import json
+
+import pandas 
+import pymc as mc
+
+from time import clock
+import pylab as pl
+import sys
+import os
+
+import dmco_methods as dmco
+reload(dmco)
+
+# model id number for data
+data_num = int(sys.argv[1])
+prior_num = int(sys.argv[2])
+year = int(sys.argv[3])
+
+# run settings
+iter=500
+burn=1
+thin=2
+# iter=20000
+# burn=10000
+# thin=10
+
+# load mortality estimates 
+mortality = pandas.read_csv('/homes/peterhm/gbd/dmco_mortality.csv')
+
+# load country model and add country-specific mortality estimates
+model = dmco.load_new_model(data_num, country, sex, cov='average')
+dmco.add_data(model, mortality, country, year)
+model, model_priors, model_t, model_mare = dmco.mvn(model, prior_num, 'consistent', country, sex, year, iter, burn, thin)
+
+# generate estimates
+dmco.save_posterior(dismod3.load_disease_model(data_num), model, country, sex, year, 
+                    ['incidence', 'prevalence', 'remission', 'excess-mortality', 'duration', 'prevalence_x_excess-mortality'])
+
+
