60eaf1a137dc8f8ec031fec23aef7a0220f98ca0
Hannah Peterson <peterhm@uw.edu>
2013-05-22 14:57:36 -0700

    enh: use newer version of python

--- dmco_fit_posterior.sh
+++ dmco_fit_posterior.sh
@@ -2,4 +2,4 @@
 
 #$ -S /bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/boost-current/lib
-/usr/local/epd-7.0-2/bin/python /homes/peterhm/gbd/dmco_fit_posterior.py $1 $2 $3 $4 $5
+/usr/local/epd-7.2-2/bin/python /homes/peterhm/gbd/dmco_fit_posterior.py $1 $2 $3 $4 $5
