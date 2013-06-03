b449c0ef13a3b53d54f90dc6a29a6c6834b2c1c4
Hannah Peterson <peterhm@uw.edu>
2013-06-03 11:26:20 -0700

    enh: automating pdf graphics

--- dmco_launch.py
+++ dmco_launch.py
@@ -1,4 +1,10 @@
-'''Run with the following arguments: data_num prior_num year'''
+'''
+Run with the following arguments: data_num prior_num year rate_type_list
+data_num : int, location of data
+prior_num : int, 
+year : int, 1990, 2005, or 2010
+rate_type_list : str, list of rate_types separated by ' ', ex. 'p i f r' 
+'''
 
 import pylab as pl
 import pandas 
@@ -18,7 +24,13 @@ country_list.remove('MAC')
 country_list.remove('PRI')
 
 # launch on cluster
+name_list = []
 for country in country_list: #['USA', 'GBR']:
     for sex in ['male', 'female']:
-        os.system('/usr/local/bin/SGE/bin/lx24-amd64/qsub -cwd /homes/peterhm/gbd/dmco_fit_posterior.sh %s %s %s %s %s' %(sys.argv[1], sys.argv[2], sys.argv[3], country, sex))
-
+        name = country + str(year) + sex
+        name_list.append(name)
+        os.system('/usr/local/bin/SGE/bin/lx24-amd64/qsub -cwd -N ' + name + ' /homes/peterhm/gbd/dmco_fit_posterior.sh %s %s %s %s %s' %(sys.argv[1], sys.argv[2], sys.argv[3], country, sex))
+        
+# creating figures in .pdf
+hold_str = '-hold_jid %s ' % ','.join(name_list)
+os.system('/usr/local/bin/SGE/bin/lx24-amd64/qsub -cwd ' + hold_str + ' /homes/peterhm/gbd/' %(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]))
---
+++ dmco_plot_fits_pdf.py
@@ -0,0 +1,17 @@
+'''
+Run with the following arguments: data_num prior_num year rate_type_list
+data_num : int, location of data
+prior_num : int, location of the priors
+year : int, 1990, 2005, or 2010
+rate_type_list : str, list of rate_types separated by ' ', ex. 'p i f r' 
+'''
+
+import matplotlib
+matplotlib.use('AGG')
+
+import sys
+
+import dmco_methods as dmco
+reload(dmco)
+
+dmco.plot_fits_pdf(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
